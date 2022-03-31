b8c3aa600cfb31c17dd03c573a3553ab5b53c887
Matthieu Pauze <matt.pauze@gmail.com>
2022-03-31 02:41:00 -0400

    Created defaults for options object

--- bar-chart.js
+++ bar-chart.js
@@ -1,1 +1,30 @@
+var options = {
+  width: "100px",
+  height: "100px",
+  valuePosition: "top",
+  barColour: "blue",
+  labelColour: "white",
+  barSpacing: "5px",
+  chartAxes: {
+    x: "X Axis",
+    y: "Y Axis"
+  },
+  labelNames: [],
+  tickInterval: "5px",
+  title: {
+    name: "My Chart",
+    fontSize: 11,
+    fontColour: "blue"
+  }
+};
 
+/**
+ * Render a bar chart horizontally, into the element passed as a parameter, using values from data, and height/width from options.
+ * @param {number[]} data
+ * @param {Object} options
+ * @param {Element} element
+ * @returns
+ */
+function drawBarChart(data, options, element) {
+  return;
+}
